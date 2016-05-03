6aa4838cd8b431690c567f01d7b22445b95689aa
Autumn Apsey <autumnapsey@gmail.com>
2016-05-03 16:08:21 -0400

    first commit

---
+++ jquery.tubular.1.0.1/js/index.js
@@ -0,0 +1,4 @@
+// $('document').ready(function() {
+//	var options = { videoId: 'xGS2Yt_59pg', start: 3 };
+//	$('#wrapper').tubular(options);
+// });
---
+++ script.js
@@ -0,0 +1,3 @@
+$('document').ready(function() {
+		$('#wrapper').tubular({videoId: 'xGS2Yt_59pg'}); 
+	});
